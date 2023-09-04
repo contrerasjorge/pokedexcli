23aede5378caf20819c828cd43a210704f8ffabb
Jorge Contreras <jorge.contreras789@gmail.com>
2023-09-04 10:19:08 -0400

    Basic implementation of catch, exit, explore, help, inspect, map and pokedex commands

---
+++ command_inspect.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func callbackInspect(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("no pokemon name provided")
+	}
+	pokemonName := args[0]
+
+	pokemon, ok := cfg.caughtPokemon[pokemonName]
+	if !ok {
+		return errors.New("you haven't caught this pokemon yet")
+	}
+
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf(" - %s: %v\n", stat.Stat.Name, stat.BaseStat)
+	}
+	fmt.Println("Type(s):")
+	for _, typ := range pokemon.Types {
+		fmt.Printf(" - %s\n", typ.Type.Name)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	"github.com/contrerasjorge/pokedexcli/internal/pokeapi"
+)
+
+type config struct {
+	pokeapiClient       pokeapi.Client
+	nextLocationAreaURL *string
+	prevLocationAreaURL *string
+	caughtPokemon       map[string]pokeapi.Pokemon
+}
 
 func main() {
-	fmt.Println("hey")
+	cfg := config{
+		pokeapiClient: pokeapi.NewClient(time.Hour),
+		caughtPokemon: make(map[string]pokeapi.Pokemon),
+	}
+
+	startRepl(&cfg)
 }
